09333a0792988862edb043b4065695035f22bf3d
kichcoin43 <k.bogdan200331@gmail.com>
2025-03-13 12:12:24 +0200

    Delete app_wsgi.py

--- app_wsgi.py
+++
@@ -1,9 +0,0 @@
-
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-
-# Простой wsgi файл для gunicorn
-from app import app
-
-# Экспортируем как application для совместимости со стандартами WSGI
-application = app
